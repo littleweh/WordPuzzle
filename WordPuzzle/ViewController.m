9b2dae7650442866b920e7ddb1cc7f5e86f71e1c
Ada Kao <adakao@zdev.local>
2018-04-13 09:41:02 +0800

    first commit

--- WordPuzzle/ViewController.m
+++ WordPuzzle/ViewController.m
@@ -9,20 +9,48 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (assign, nonatomic, readwrite) int wordBoxSize;
+@property (strong, nonatomic, readwrite) NSMutableArray* words2DArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.wordBoxSize = 8;
+    
+    NSMutableArray* numbers = [NSMutableArray arrayWithCapacity:64];
+    for (int i =0; i<64; i++) {
+        numbers[i] = [[NSString alloc]initWithFormat:@"%i", i+1];
+    }
+    self.words2DArray = [self setWords2DArrayWithSquareLength:self.wordBoxSize Words:numbers];
+    [self showWords2DArrayContent];
+    
+    
+    
 }
 
+-(NSMutableArray*) setWords2DArrayWithSquareLength: (int) boxLength Words: (NSMutableArray*) materials {
+    NSMutableArray *words2DArray = [NSMutableArray array];
+    NSInteger index = 0;
+    for (int row = 0; row < boxLength; row++) {
+        NSMutableArray *rowArray = [NSMutableArray array];
+        for (int column =0; column < boxLength; column++) {
+            [rowArray addObject:materials[index++]];
+        }
+        [words2DArray addObject:rowArray];
+    }
+    return words2DArray;
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// MARK: FOR TEST
+-(void) showWords2DArrayContent {
+    for (int i = 0; i < self.words2DArray.count; i++) {
+        NSMutableArray *columnArray = self.words2DArray[i];
+        for (int j = 0; j < columnArray.count; j++) {
+            NSLog(@"%@", self.self.words2DArray[i][j]);
+        }
+    }
 }
 
 
